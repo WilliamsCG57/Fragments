8f77e61f6ffbbb0dc38a8ef71d052da4b62c519b
Williams Ccompi <ccompiguerrerow@gmail.com>
2024-09-07 11:34:30 -0500

    Initial commit

---
+++ app/src/main/java/dev/williamscg/fragments/ui/fragments/MusicalFragment.kt
@@ -0,0 +1,20 @@
+package dev.williamscg.fragments.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.williamscg.fragments.R
+
+
+class MusicalFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_musica, container, false)
+    }
+}

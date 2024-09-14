ccf9819fc3c4d555ad7543dc426f55bb9bcc0107
Williams Ccompi <ccompiguerrerow@gmail.com>
2024-09-14 12:33:24 -0500

    ItemMusica en MusicaFragment

---
+++ app/src/main/java/dev/williamscg/fragments/ui/model/SongModel.kt
@@ -0,0 +1,9 @@
+package dev.williamscg.fragments.ui.model
+
+data class SongModel(
+    val id: Int,
+    val songName: String,
+    val album: String,
+    val countPlay: String,
+    val sonTime: String
+)

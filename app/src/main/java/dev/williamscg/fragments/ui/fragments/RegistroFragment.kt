b16703f5b35b21cd75c8ebb30769859a21ba994f
Williams Ccompi <ccompiguerrerow@gmail.com>
2024-09-14 12:10:04 -0500

    Principal and RegistroFragment

--- app/src/main/java/dev/williamscg/fragments/ui/fragments/RegistroFragment.kt
+++ app/src/main/java/dev/williamscg/fragments/ui/fragments/RegistroFragment.kt
@@ -5,6 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Spinner
+import android.widget.Toast
 import dev.williamscg.fragments.R
 
 
@@ -15,6 +24,50 @@ class RegistroFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_registro, container, false)
+
+        val btSave: Button =view.findViewById(R.id.btSave)
+        val etFullName: EditText = view.findViewById(R.id.etFullName)
+        val spCountry: Spinner = view.findViewById(R.id.spCountry)
+        val etEmail: EditText = view.findViewById(R.id.etEmail)
+        val rgGender: RadioGroup = view.findViewById(R.id.rgGender)
+        val ckLicense: CheckBox = view.findViewById(R.id.ckLicense)
+        val ckCar: CheckBox = view.findViewById(R.id.ckCar)
+
+        //Loading Spinner
+        ArrayAdapter.createFromResource(requireContext(),
+            R.array.country_array,
+            android.R.layout.simple_spinner_item).also{
+                adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spCountry.adapter = adapter
+        }
+
+        var spCountryValue = ""
+        spCountry.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                spCountryValue = parent?.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        btSave.setOnClickListener{
+            val fullNameValue = etFullName.text.toString()
+            val emailValue = etEmail.text.toString()
+            var intSelectedButton = rgGender!!.checkedRadioButtonId
+            var rbSelected: RadioButton = view.findViewById(intSelectedButton)
+            val genderValue = rbSelected.text
+
+            val allValues = "Full name: $fullNameValue \nEmail: $emailValue \nGender: $genderValue "+
+                    "\nCountry: $spCountryValue \nLicense: ${ckLicense.isChecked}" +
+                    "\nCar: ${ckCar.isChecked}"
+
+            Toast.makeText(requireContext(),allValues, Toast.LENGTH_LONG).show()
+        }
+
+        return view
     }
 }
